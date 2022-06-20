9b2f27b92baa440ac96e2b8d5f3c4807d52bab95
Kevin Gnanaraj <kevin.gnanaraj@hotmail.com>
2022-06-20 15:37:45 -0400

    Started layout and home page, added files for website theme under /static, and tested to make sure it runs and renders in flask. Need to set up venv for Python and install flask before running

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from helpers import apology
+
+# Configure application
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template("index.html")
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+if __name__ == "__main__":
+    app.run()
