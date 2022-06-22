6f074d3435518c669044b508033cc2ca7fd090e2
Kevin Gnanaraj <kevin.gnanaraj@hotmail.com>
2022-06-21 20:11:50 -0400

    Added routes and html files for some non-homepage links

--- app.py
+++ app.py
@@ -5,15 +5,36 @@ from helpers import apology
 # Configure application
 app = Flask(__name__)
 
+# Website page routes
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/research")
+def research():
+    return render_template("research.html")
 
 def errorhandler(e):
     """Handle error"""
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
+
+# To be build out after static part of website is complete and live
+# Requires libraries that have not been installed or imported yet, so leave commented for later
+
+# @app.route("/surgical_records")
+# @login_required
+# def surgical_records():
+#     return render_template("surgical_records.html")
 
 if __name__ == "__main__":
     app.run()
